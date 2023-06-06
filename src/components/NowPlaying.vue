505a49d7aaee8b31b34ecb1dde770cf381b5d41c
dooz-bag <135302683+dooz-bag@users.noreply.github.com>
2023-06-06 14:17:03 -0500

    Update NowPlaying.vue
    trying copy paste

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -18,7 +18,8 @@
       </div>
     </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
-      <h1 class="now-playing__idle-heading">No music is playing 😔</h1>
+      <h1 class="now-playing__track" v-text="player.trackTitle"></h1>
+      <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
     </div>
   </div>
 </template>
